cdd8df186e1006d08096b53b087cb5f0860e4a27
Azevor <gildotiago@gmail.com>
2021-07-06 11:59:46 -0300

    Teste 4.

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 # coding: utf-8
 from flask import Flask, render_template
+from flask_socketio import SocketIO, emit, send
 
 
 app = Flask(__name__)
+io = SocketIO(app)
+messages = []
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
+@io.on('sendMessage')
+def send_message_handler(msg):
+    messages.append(msg) # Armazenando histórico de mensagens
+    emit('getMessage', msg, broadcast=True)
+
+@io.on('message')
+def message_handler(msg):
+    send(messages)
 
 if __name__ == '__main__':
 	app.run()
