e31e1f754f922930ddd90302f286281b6746d47e
Azevor <gildotiago@gmail.com>
2021-07-06 11:48:06 -0300

    Teste 2.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return ("Teste 1", 200)
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
