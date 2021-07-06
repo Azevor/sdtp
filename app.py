215e022cb38d83de7815edfba0d2e082f20f0886
Azevor <gildotiago@gmail.com>
2021-07-06 09:27:11 -0300

    Teste

--- app.py
+++ app.py
@@ -3,32 +3,34 @@
 Importação do flask e flask_socketio
 '''
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit, send
+# from flask_socketio import SocketIO, emit, send
 
 '''
 Instancia de Flask passado como parâmetro para o socket
 '''
 app = Flask(__name__)
-io = SocketIO(app)
+# io = SocketIO(app)
 
-messages = []
+# messages = []
 
 
 @app.route("/")
 def home():
     return render_template("chat.html")
 
-@io.on('sendMessage')
-def send_message_handler(msg):
-    messages.append(msg) # Armazenando histórico de mensagens
-    emit('getMessage', msg, broadcast=True)
+app.run()
 
-@io.on('message')
-def message_handler(msg):
-    send(messages)
+# @io.on('sendMessage')
+# def send_message_handler(msg):
+    # messages.append(msg) # Armazenando histórico de mensagens
+    # emit('getMessage', msg, broadcast=True)
+
+# @io.on('message')
+# def message_handler(msg):
+#     send(messages)
 
 '''
 Arquivo principal (rodar a aplicação)
 '''
-if __name__ == "__main__":
-    io.run(app, host='https://sdtp.azurewebsites.net', port='80', debug=True)
+# if __name__ == "__main__":
+    # io.run(app, host='https://sdtp.azurewebsites.net', port='80', debug=True)
