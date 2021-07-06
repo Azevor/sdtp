bc83f41825f9abdb7c33b2f5d703172183908df1
Azevor <gildotiago@gmail.com>
2021-07-06 20:42:12 -0300

    Teste.

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def message_handler(msg):
 
 if __name__ == '__main__':
 	# app.run()
-	io.run(app, host='https://sdtp.azurewebsites.net', port='80', debug=True)
+	io.run(app, host='http://sdtp.azurewebsites.net/', port='80', debug=True)
