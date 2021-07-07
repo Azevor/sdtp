62abb4cd015a66b987880072450379905c1a1a2d
Azevor <gildotiago@gmail.com>
2021-07-06 21:46:01 -0300

    Recovery

---
+++ app.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit, send
+
+
+app = Flask(__name__)
+io = SocketIO(app)
+messages = []
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@io.on('sendMessage')
+def send_message_handler(msg):
+    messages.append(msg) # Armazenando histórico de mensagens
+    emit('getMessage', msg, broadcast=True)
+
+@io.on('message')
+def message_handler(msg):
+    send(messages)
+
+if __name__ == '__main__':
+	# app.run()
+	io.run(app, host='http://sdtp.azurewebsites.net/', port='80', debug=True)
